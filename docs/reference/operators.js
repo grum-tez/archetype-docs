8ce5bbfcc5b2c29f93ab2c9ab203c2e1f0b3abcd
Benoit Rognier <benoit.rognier@edukera.com>
2022-03-17 15:09:59 +0100

    fix

--- docs/reference/operators.js
+++ docs/reference/operators.js
@@ -8,12 +8,12 @@ export const operators = {
       { typa: 'int',          typb : 'int',          typr: 'int' },
       { typa: 'rational',     typb : 'rational',     typr: 'rational' },
       { typa: 'string',       typb : 'string',       typr: 'string' },
+      { typa: 'date', typb : 'duration', typr: 'date' },
       { typa: 'bls12_381_fr', typb : 'bls12_381_fr', typr: 'bls12_381_fr' },
       { typa: 'bls12_381_g1', typb : 'bls12_381_g1', typr: 'bls12_381_g1' },
       { typa: 'bls12_381_g2', typb : 'bls12_381_g2', typr: 'bls12_381_g2' },
     ],
     promotions: [
-      { typa: 'date', typb : 'duration', typr: 'date' },
       { typa: 'nat', typb : 'int', typr: 'int' },
       { typa: 'int', typb : 'nat', typr: 'int' },
     ]
