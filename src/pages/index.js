cfbca57388fe5ed0790fca40eb54180d3ad64195
Benoit Rognier <benoit.rognier@edukera.com>
2022-06-16 17:59:19 +0200

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -279,7 +279,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Smart Contract Archetype Language">
         <StyledEngineProvider injectFirst>
           <Landing />
