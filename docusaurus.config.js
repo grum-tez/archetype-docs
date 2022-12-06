01eb753329dc3cc231bc359236c60c548b0c8de8
Benoit Rognier <benoit.rognier@edukera.com>
2022-12-06 16:01:13 +0100

    fix version

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -106,7 +106,7 @@ const config = {
             label: 'DApps',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
-          {to: '/docs/installation', label: 'latest: 1.3.5', position: 'right'},
+          {to: '/docs/installation', label: 'latest: 1.4.0', position: 'right'},
           {
             href: 'https://tezos.com',
             label: 'Tezos',
