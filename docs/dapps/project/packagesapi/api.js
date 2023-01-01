41d74e9918a49267078f4715013bc8565a8a40b1
Benoit Rognier <benoit.rognier@edukera.com>
2023-01-01 09:36:43 +0100

    fix

--- docs/dapps/project/packagesapi/api.js
+++ docs/dapps/project/packagesapi/api.js
@@ -2,61 +2,117 @@ import React from "react"
 import Link from '@docusaurus/Link';
 
 const prefix = "/docs/tests/apis/types#"
+const exp_prefix = "/docs/tests/apis/experiment#"
 
 export const dapp = {
   call: {
     sig: 'todo',
     ref: 'callc-e-a-p',
-    desc: <div>TODO</div>,
+    desc: <div>Low-level <Link to="https://www.typescriptlang.org/docs/handbook/release-notes/typescript-1-7.html"><i>asynchronous</i></Link> call to contract's entry points used by <Link to="/docs/tests/binding#entry-points">contract binding</Link>.</div>,
     parameters: [
       {
         type: 'string',
-        alias: 'a',
+        alias: 'c',
         desc: <div>Contract address</div>,
         withLink: false
-      }
+      },
+      {
+        type: 'string',
+        alias: 'e',
+        desc: <div>entry point name</div>,
+        withLink: false
+      },
+      {
+        type: 'Micheline',
+        alias: 'a',
+        desc: <div>entry point argument</div>,
+        prefix : prefix
+      },
+      {
+        type: 'Parameters',
+        alias: 'p',
+        desc: <div>Call parameter (<Link to="/docs/reference/expressions/constants#caller"><code>caller</code></Link> and <Link to="/docs/reference/expressions/constants#transferred"><code>transferred</code></Link>)</div>,
+        withLink: false
+      },
     ],
     returns: {
-      type: 'any',
-      desc: <div>TODO</div>,
-      withLink: false
-    }
+      type: 'CallResult',
+      desc: <div>Various data about transaction.</div>,
+      prefix : exp_prefix
+    },
+    related: [
+      { keyword: 'exec_batch', link:'/docs/dapps/project/packagesapi/#exec_batchcps-p' }
+    ],
+    fails: [
+      {
+        keyword: 'Error',
+        desc: <div>An error is thrown if the transaction fails.</div>
+      }
+    ]
   },
   deploy: {
     sig: 'todo',
     ref: 'deploypath-params-p',
-    desc: <div>TODO</div>,
+    desc: <div>Low-level <Link to="https://www.typescriptlang.org/docs/handbook/release-notes/typescript-1-7.html"><i>asynchronous</i></Link> function to deploy an Archetype contract, called by the <Link to="/docs/tests/binding#deploy"><code>deploy</code></Link> method of the contract's binding.</div>,
     parameters: [
       {
         type: 'string',
-        alias: 'a',
-        desc: <div>Contract address</div>,
+        alias: 'path',
+        desc: <div>Path to contract (relative to command's execution folder)</div>,
         withLink: false
-      }
+      },
+      {
+        type: 'any',
+        alias: 'params',
+        desc: <div>Contract intital storage values</div>,
+        prefix: prefix
+      },
+      {
+        type: 'Parameters',
+        alias: 'p',
+        desc: <div>Call parameter (<Link to="/docs/reference/expressions/constants#caller"><code>caller</code></Link> and <Link to="/docs/reference/expressions/constants#transferred"><code>transferred</code></Link>)</div>,
+        withLink: false
+      },
     ],
+    showfail : false,
     returns: {
-      type: 'any',
-      desc: <div>TODO</div>,
+      type: 'DeployResult',
+      desc: <div>Various data about deployment transaction.</div>,
       withLink: false
     }
   },
   exec_batch: {
     sig: 'todo',
     ref: 'exec_batchcps-p',
-    desc: <div>TODO</div>,
+    desc: <div><Link to="https://www.typescriptlang.org/docs/handbook/release-notes/typescript-1-7.html"><i>Asynchronous</i></Link> function to execute calls to entry points in <i>batch</i> mode. See <Link to="/docs/tests/binding#entry-points-parameters">here</Link> for more information.</div>,
     parameters: [
       {
-        type: 'string',
-        alias: 'a',
-        desc: <div>Contract address</div>,
+        type: 'Array<CallParameter>',
+        alias: 'cps',
+        desc: <div>Array of <Link to="/docs/tests/apis/types#callparameter"><code>CallParameter</code></Link> values</div>,
         withLink: false
-      }
+      },
+      {
+        type: 'Parameters',
+        alias: 'p',
+        desc: <div>Call parameter (<Link to="/docs/reference/expressions/constants#caller"><code>caller</code></Link> and <Link to="/docs/reference/expressions/constants#transferred"><code>transferred</code></Link>)</div>,
+        withLink: false
+      },
     ],
     returns: {
-      type: 'any',
-      desc: <div>TODO</div>,
-      withLink: false
-    }
+      type: 'CallResult',
+      desc: <div>Transaction information.</div>,
+      prefix: exp_prefix
+    },
+    related: [
+      { keyword: 'call', link:'/docs/tests/apis/experiment#callc-e-a-p' }
+    ],
+    fails: [
+      {
+        keyword: 'Error',
+        desc: <div>An error is thrown if one the transactions fails.</div>
+      }
+    ]
   },
   exec_getter: {
     sig: 'todo',
@@ -78,38 +134,58 @@ export const dapp = {
   },
   exec_view: {
     sig: 'todo',
-    ref: 'exec_viewc-e-a-p',
-    desc: <div>TODO</div>,
+    ref: 'exec_viewc-n-a-p',
+    desc: <div>Low-level <Link to="https://www.typescriptlang.org/docs/handbook/release-notes/typescript-1-7.html"><i>asynchronous</i></Link> function to call on on-chain contract's <Link to="/docs/reference/declarations/view"><code>view</code></Link>. It is used by the contract binding to <Link to="/docs/tests/binding#views">call a view</Link>.</div>,
     parameters: [
       {
-        type: 'string',
+        type: 'Address',
         alias: 'a',
         desc: <div>Contract address</div>,
-        withLink: false
-      }
+        prefix: prefix
+      },
+      {
+        type: 'string',
+        alias: 'n',
+        desc: <div>View's name</div>,
+        withLink : false
+      },
+      {
+        type: 'Micheline',
+        alias: 'a',
+        desc: <div>View argument</div>,
+        prefix : prefix
+      },
+      {
+        type: 'Parameters',
+        alias: 'p',
+        desc: <div>Parameters</div>,
+        prefix: prefix
+      },
     ],
+    showfail : false,
     returns: {
-      type: 'any',
-      desc: <div>TODO</div>,
+      type: 'ViewResult',
+      desc: <div>View's returned value</div>,
       withLink: false
     }
   },
   get_balance: {
     sig: 'todo',
     ref: 'get_balancea',
-    desc: <div>TODO</div>,
+    desc: <div><Link to="https://www.typescriptlang.org/docs/handbook/release-notes/typescript-1-7.html"><i>Asynchronous</i></Link> function to retrieve contract's balance</div>,
     parameters: [
       {
-        type: 'string',
+        type: 'Address',
         alias: 'a',
         desc: <div>Contract address</div>,
-        withLink: false
+        prefix: prefix
       }
     ],
+    showfail : false,
     returns: {
-      type: 'any',
-      desc: <div>TODO</div>,
-      withLink: false
+      type: 'Tez',
+      desc: <div>Contract balance</div>,
+      prefix: prefix
     }
   },
   get_big_map_value : {
@@ -156,25 +232,44 @@ export const dapp = {
   get_call_param: {
     sig: 'todo',
     ref: 'get_call_paramc-e-a-p',
-    desc: <div>TODO</div>,
+    desc: <div>Low-level <Link to="https://www.typescriptlang.org/docs/handbook/release-notes/typescript-1-7.html"><i>asynchronous</i></Link> function to get the transaction parameters to call an entry point. It is used by the contract's binding to get entry point transaction parameters for <Link to="/docs/tests/binding#entry-points-parameters">batch</Link> execution of entry points.</div>,
     parameters: [
       {
         type: 'string',
-        alias: 'a',
+        alias: 'c',
         desc: <div>Contract address</div>,
         withLink: false
-      }
+      },
+      {
+        type: 'string',
+        alias: 'e',
+        desc: <div>Entry point name</div>,
+        withLink: false
+      },
+      {
+        type: 'Micheline',
+        alias: 'a',
+        desc: <div>Entry point argument</div>,
+        prefix: prefix
+      },
+      {
+        type: 'Parameters',
+        alias: 'p',
+        desc: <div>Parameters</div>,
+        prefix: prefix
+      },
     ],
+    showfail : false,
     returns: {
-      type: 'any',
-      desc: <div>TODO</div>,
+      type: 'CallParameter',
+      desc: <div>Transaction parameter</div>,
       withLink: false
     }
   },
   get_storage: {
     sig: 'todo',
     ref: 'get_storagea',
-    desc: <div>TODO</div>,
+    desc: <div><Link to="https://www.typescriptlang.org/docs/handbook/release-notes/typescript-1-7.html"><i>Asynchronous</i></Link> function that returns the <Link to="https://tezostaquito.io/">Taquito</Link> storage of a contract.</div>,
     parameters: [
       {
         type: 'string',
@@ -183,29 +278,26 @@ export const dapp = {
         withLink: false
       }
     ],
+    showfail : false,
     returns: {
       type: 'any',
-      desc: <div>TODO</div>,
+      desc: <div>Taquito's formatted contract storage.</div>,
       withLink: false
     }
   },
   set_binder_tezos_toolkit: {
     sig: 'todo',
     ref: 'set_binder_tezos_toolkittk',
-    desc: <div>TODO</div>,
+    desc: <div>Sets the <Link to="/docs/dapps/project/#tezos-library">Taquito Toolkit</Link>. It is used by the <Link to="/docs/tests/binding">binding</Link> object to make calls to the blockchain.</div>,
     parameters: [
       {
-        type: 'string',
-        alias: 'a',
-        desc: <div>Contract address</div>,
+        type: 'TezosToolkit',
+        alias: 'tk',
+        desc: <div><Link to="https://tezostaquito.io/docs/quick_start/#import-tezostoolkit-from-taquitotaquito-and-instantiate-it">Taquito</Link>'s toolkit</div>,
         withLink: false
       }
     ],
-    returns: {
-      type: 'any',
-      desc: <div>TODO</div>,
-      withLink: false
-    }
+    showfail: false
   },
 }
 
